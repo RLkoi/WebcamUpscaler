6007de8dff30e8c645cde953953107125723c4d0
Retrogameing11 <78034237+RLkoi@users.noreply.github.com>
2026-08-25 01:37:40 +0100

    Delete Fsr3VirtualCamera V1.1.0/gpuobs directory

--- Fsr3VirtualCamera V1.1.0/gpuobs/InstallerNative/resource.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-#define IDR_APP_EXE 101
-#define IDR_PLUGIN_DLL 102
-#define IDR_LOCALE_INI 103
--- Fsr3VirtualCamera V1.1.0/gpuobs/Uninstall-OBS-Plugin.ps1
+++
@@ -1,8 +0,0 @@
-$ErrorActionPreference = "Stop"
-$dest = Join-Path $env:ProgramData "obs-studio\plugins\fsr-camera-source"
-if (Test-Path $dest) {
-    Remove-Item $dest -Recurse -Force
-    Write-Host "FSR Camera OBS plugin removed. Restart OBS."
-} else {
-    Write-Host "FSR Camera OBS plugin is not installed in ProgramData."
-}
